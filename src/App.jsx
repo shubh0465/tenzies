0cd477dacd464c7cb806875c590cd803a3e13f91
shubh0465 <55250438+shubh0465@users.noreply.github.com>
2023-02-06 15:28:29 +0530

    Added Winning game animation and button start new game

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,80 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import Dice from './Components/Dice'
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 function App() {
 
+  const [allNewDiceArray, setAllNewDiceArray] = useState(allNewDice())
+
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    const val = allNewDiceArray[0].value
+    const res = allNewDiceArray.every((item)=>{
+      return item.isHeld === true && item.value===val;
+    })
+    if(res){
+      setTenzies(true);
+      console.log("You Won!")
+    }
+  }, [allNewDiceArray])
+  
+
+  function generateDie() {
+    return { value: Math.floor(Math.random() * 6) + 1, isHeld: false, id: nanoid() }
+  }
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({ value: Math.floor(Math.random() * 6) + 1, isHeld: false, id: nanoid() })
+      newDice.push(generateDie())
     }
     return newDice
   }
 
-  const [allNewDiceArray, setAllNewDiceArray] = useState(allNewDice())
-  
   function generateNewArray() {
-    setAllNewDiceArray(oldArray =>{
-      return oldArray.map(item=>{
-        return item.isHeld===false ? {...item, value : Math.floor(Math.random() * 6) + 1} : item;
+    setAllNewDiceArray(oldArray => {
+      return oldArray.map(item => {
+        return item.isHeld ? item : generateDie();
       })
     })
   }
-  
+
+  function generateNewGame(){
+    setTenzies(false)
+    setAllNewDiceArray(oldArray => {
+      return oldArray.map(()=> {
+        return generateDie();
+      })
+    })
+  }
+
   function holdDice(id) {
-    setAllNewDiceArray(oldDice =>{
-      return oldDice.map(item=>{
-        return item.id === id ? {...item, isHeld:!item.isHeld}:item;
+    setAllNewDiceArray(oldDice => {
+      return oldDice.map(item => {
+        return item.id === id ? { ...item, isHeld: !item.isHeld } : item;
       })
     })
   }
-  
+
   const diceElement = allNewDiceArray.map((item) => {
     return <Dice key={item.id} value={item.value} isHeld={item.isHeld} holdDice={() => holdDice(item.id)} />
   })
 
-  
+
   return (
     <main className='main-container'>
+      {tenzies && <Confetti/>}
       <div className='container'>
         <div>
           <h1>Tenzies</h1>
-          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+          <p className='information'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         </div>
         <div className='dice-container'>
           {diceElement}
         </div>
-        <button onClick={generateNewArray} className="roll-button">Roll</button>
+        {tenzies? <button onClick={generateNewGame} className="roll-button">New Game</button> : <button onClick={generateNewArray} className="roll-button">Roll</button>}
       </div>
     </main>
   )
