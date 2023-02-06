06dcc7ee927a4f84f9b033ba31496724d3c4fa70
shubh0465 <55250438+shubh0465@users.noreply.github.com>
2023-02-06 13:08:15 +0530

    hold dice property added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
 import { useState } from 'react'
 import './App.css'
 import Dice from './Components/Dice'
-
+import { nanoid } from 'nanoid';
 function App() {
-  function allNewDice(){
-    const newDice =[];
-    for(let i = 0 ; i<10; i++){
-      newDice.push(Math.floor(Math.random()*6)+1)
+
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({ value: Math.floor(Math.random() * 6) + 1, isHeld: false, id: nanoid() })
     }
     return newDice
   }
+
   const [allNewDiceArray, setAllNewDiceArray] = useState(allNewDice())
-  const diceElement = allNewDiceArray.map((item,index)=>{
-    return <Dice key={index} value={item}/>
+  
+  function generateNewArray() {
+    setAllNewDiceArray(allNewDice())
+  }
+  
+  function holdDice(id) {
+    setAllNewDiceArray(oldDice =>{
+      return oldDice.map(item=>{
+        return item.id === id ? {...item, isHeld:!item.isHeld}:item;
+      })
+    })
+  }
+  
+  const diceElement = allNewDiceArray.map((item) => {
+    return <Dice key={item.id} value={item.value} isHeld={item.isHeld} holdDice={() => holdDice(item.id)} />
   })
+
+  
   return (
     <main className='main-container'>
       <div className='container'>
+        <div>
+          <h1>Tenzies</h1>
+          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        </div>
         <div className='dice-container'>
           {diceElement}
         </div>
+        <button onClick={generateNewArray} className="roll-button">Roll</button>
       </div>
     </main>
   )
--- src/Components/Dice.jsx
+++ src/Components/Dice.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Dice(props) {
   return (
-    <div className='dice'>
+    <div className='dice' style={{backgroundColor : props.isHeld?"#59E391":"#ffffff"}} onClick={props.holdDice}>
         <h2>{props.value}</h2>
     </div>
   )
